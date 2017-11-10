9012ed4fe088589576399f82e4c3e0c1b805399c
Mohamed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-11-10 20:01:16 +0200

    add autoreeload to html files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 'use strict';
 
 require('./hmr');
+require('./index');
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 'use strict';
 
 const webpack = require('webpack');
+const ReloadHtmlWebpackPlugin = require('reload-html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 
 module.exports = {
   context: __dirname,
   entry: './app.js',
   plugins: [
-    new webpack.NamedModulesPlugin()
-  ]
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({template: './index.html'}),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module:{
+    loaders:[
+        {
+            test: /\.html$/,
+            loader: 'raw-loader'
+        }
+    ]
+  }
 };
